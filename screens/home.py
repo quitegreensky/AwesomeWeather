2ec28f8a954ab3eeba0ec896740b7b9595d69b8a
quitegreensky <quitegreensky@gmail.com>
2020-12-01 21:08:46 +0330

    Revert "toolbar fix"
    This reverts commit 2cee3c504d55c91570a1419be81ff2591ed4a65d.

--- screens/home.py
+++ screens/home.py
@@ -31,8 +31,6 @@ Builder.load_string(
                     title: "Weather"
                     md_bg_color: 0,0,0,0
                     anchor_title: "center"
-                    _hard_shadow_a : 0
-                    _soft_shadow_a : 0
                     left_action_items: [("cog-outline", lambda x: app.show_screen("Setting"))]
                     right_action_items: [("dots-vertical", lambda x: None)]
 
@@ -40,9 +38,9 @@ Builder.load_string(
                     id: weather
 
                 BoxLayout:
-                    padding: dp(10)
+                    padding: dp(20)
                     orientation: "vertical"
-                    spacing: dp(5)
+                    spacing: dp(20)
 
                     canvas.before:
                         Color:
@@ -60,6 +58,7 @@ Builder.load_string(
                         text_color: 0.1,5,0.4,1
                         height: dp(60)
 
+                    BoxLayout:
 
         AKSwipeMenuTopContent:
             size_hint_y: None
